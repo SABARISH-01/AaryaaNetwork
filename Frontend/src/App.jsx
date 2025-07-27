3c5909cc7d59d56b791100e95509411ae5cfdb28
SABARISH-01 <sabarishvengadesh64@gmail.com>
2025-07-28 02:57:34 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import WebRouter from './WebRouter';
-import SmoothScroll from './Components/Scroll/SmoothScroll.jsx';
-import './App.css';
-import AdminApp from './Admin/AdminApp.jsx';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import WebRouter from "./WebRouter";
+import SmoothScroll from "./Components/Scroll/SmoothScroll.jsx";
+import "./App.css";
+import AdminApp from "./Admin/AdminApp.jsx";
 
 function App() {
   return (
-    <SmoothScroll>
-      <Routes>
-        <Route path="/*" element={<WebRouter />} />
-         <Route path="/admin/*"  element={<AdminApp/>}/>
-      </Routes>
-     </SmoothScroll> 
+    <>
+      <SmoothScroll>
+        <Routes>
+          <Route path="/*" element={<WebRouter />} />
+          <Route path="/admin/*" element={<AdminApp />} />
+        </Routes>
+      </SmoothScroll>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop
+        closeButton={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 }
 
